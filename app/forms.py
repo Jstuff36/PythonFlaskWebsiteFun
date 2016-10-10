bf627946a143fbecf1cfdc5cb990cae4628a60e6
Jstuff36 <mizzou_fan10@hotmail.com>
2016-10-10 14:14:55 -0500

    Added Login Screen Non Functional

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask.ext.wtf import Form 
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+	openid = StringField('openid', validators = [DataRequired()]) 
+	
+	'''The DataRequired import is a validator, a function that can be attached to a field 
+	to perform validation on the data submitted by the user. The DataRequired validator 
+	simply checks that the field is not submitted empty. There are many more validators 
+	included with Flask-WTF, we will use some more in the future.'''
+	
+	remember_me = BooleanField('remember_me', default = False)
